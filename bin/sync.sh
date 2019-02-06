c004933cead2510db0f1740d7f4ad6b1c090610d
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2019-02-06 15:37:50 -0800

    Moved sync script out of dev VM into hes-common project - it was causing too many problems running automatically whenever the Dev VM was launched.

---
+++ bin/sync.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Get the directory in which this script file resides
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+_sync(){
+    rsync -rtv --exclude=".git" /${DIR}/.. $1/vendor/pnnl
+}
+
+sync_all(){
+    ROOT_DIR=${DIR}/../..
+    _sync ${ROOT_DIR}/laravel-hes-gui
+    _sync ${ROOT_DIR}/hes-api
+    _sync ${ROOT_DIR}/hes-tests
+}
+
+# Start a daemon that will synchronize the hes-common project to projects that depend on it every two seconds
+while :; do clear; sync_all; sleep 2; done
