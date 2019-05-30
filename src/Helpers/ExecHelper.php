4b177b9d6dec889fc6e10fe2b6aef072495ecda1
Davis, Prescott H <prescott.davis@pnnl.gov>
2019-05-30 11:59:06 -0700

    Add assertGitIsInstalled method

--- src/Helpers/ExecHelper.php
+++ src/Helpers/ExecHelper.php
@@ -13,9 +13,29 @@ class ExecHelper
      */
     public static function assertNodeIsInstalled()
     {
-        exec('which node 2>&1', $output);
+        self::assertIsInstalled('node');
+    }
+    
+    /**
+     * Ensure git is installed before attempting to call command
+     *
+     * @throws UserSafeException
+     */
+    public static function assertGitIsInstalled()
+    {
+        self::assertIsInstalled('git');
+    }
+    
+    /**
+     * Ensure given command tool is installed before attempting to call command
+     *
+     * @throws UserSafeException
+     */
+    public static function assertIsInstalled($command)
+    {
+        exec("which $command 2>&1", $output);
         if(empty($output)){
-            throw new UserSafeException('Node is not installed.');
+            throw new UserSafeException("$command is not installed.");
         }
     }
 }
