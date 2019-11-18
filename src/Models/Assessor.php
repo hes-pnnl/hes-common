aaf3da32bef4c07d1ed93cf5295f81b51f4841a5
Yan Zhao <yan.zhao@pnnl.gov>
2019-11-18 13:03:32 -0800

    Update Assessor.php

--- src/Models/Assessor.php
+++ src/Models/Assessor.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace HESCommon\Models;
+
 /**
  * Class Assessor - Contains the Assessor information.
  */
@@ -30,11 +32,27 @@ class Assessor extends Model
     /** @var \Datetime|null */
     protected $roleDeactivated;
 
+    /** @var string */
+    private $city;
+
+    /** @var string */
+    private $state;
+
+    /** @var string */
+    private $zip;
+
+    /** @var string */
+    protected $phone;
+
+    /** @var string */
+    protected $website;
+
     //Getter methods
+
     /**
      * @return int|null
      */
-    public function getId() : ?int
+    public function getId(): ?int
     {
         return $this->id;
     }
@@ -42,7 +60,7 @@ class Assessor extends Model
     /**
      * @return string
      */
-    public function getName() : string
+    public function getName(): string
     {
         return $this->name;
     }
@@ -50,7 +68,7 @@ class Assessor extends Model
     /**
      * @return string
      */
-    public function getFullName() : string
+    public function getFullName(): string
     {
         return $this->fullName;
     }
@@ -58,7 +76,7 @@ class Assessor extends Model
     /**
      * @return string
      */
-    public function getEmail() : string
+    public function getEmail(): string
     {
         return $this->email;
     }
@@ -66,7 +84,7 @@ class Assessor extends Model
     /**
      * @return int|null
      */
-    public function getHesPartnerId() : ?int
+    public function getHesPartnerId(): ?int
     {
         return $this->hesPartnerId;
     }
@@ -74,7 +92,7 @@ class Assessor extends Model
     /**
      * @return \DateTime|null
      */
-    public function getRoleCreated() : ?\DateTime
+    public function getRoleCreated(): ?\DateTime
     {
         return $this->roleCreated;
     }
@@ -82,7 +100,7 @@ class Assessor extends Model
     /**
      * @return \DateTime|null
      */
-    public function getRoleUpdated() : ?\DateTime
+    public function getRoleUpdated(): ?\DateTime
     {
         return $this->roleUpdated;
     }
@@ -90,57 +108,101 @@ class Assessor extends Model
     /**
      * @return \DateTime|null
      */
-    public function getRoleDeactivated() : ?\DateTime
+    public function getRoleDeactivated(): ?\DateTime
     {
         return $this->roleDeactivated;
     }
 
-    //Setter methods
+
     /**
-     * @return Assessor
-     * @param int $id
+     * @return string
      */
-    public function setId(int $id) : Assessor
+    public function getCity(): String
+    {
+        return $this->city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState(): String
+    {
+        return $this->state;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getZip(): String
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): String
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebsite(): String
+    {
+        return $this->website;
+    }
+
+
+    //Setter methods
+
+    /**
+     * @param int $id
+     * @return Assessor
+     */
+    public function setId(int $id): Assessor
     {
         $this->id = $id;
         return $this;
     }
 
     /**
-     * @return Assessor
      * @param string $name
+     * @return Assessor
      */
-    public function setName(string $name) : Assessor
+    public function setName(string $name): Assessor
     {
         $this->name = $name;
         return $this;
     }
 
     /**
-     * @return Assessor
      * @param string $fullName
+     * @return Assessor
      */
-    public function setFullName(string $fullName) : Assessor
+    public function setFullName(string $fullName): Assessor
     {
         $this->fullName = $fullName;
         return $this;
     }
 
     /**
-     * @return Assessor
      * @param string $email
+     * @return Assessor
      */
-    public function setEmail(string $email) : Assessor
+    public function setEmail(string $email): Assessor
     {
         $this->email = $email;
         return $this;
     }
 
     /**
-     * @return Assessor
      * @param int|null $hesPartnerId
+     * @return Assessor
      */
-    public function setHesPartnerId(?int $hesPartnerId) : Assessor
+    public function setHesPartnerId(?int $hesPartnerId): Assessor
     {
         $this->hesPartnerId = $hesPartnerId;
         return $this;
@@ -150,7 +212,7 @@ class Assessor extends Model
      * @param \DateTime|null $roleCreated
      * @return Assessor
      */
-    public function setRoleCreated(?\DateTime $roleCreated) : Assessor
+    public function setRoleCreated(?\DateTime $roleCreated): Assessor
     {
         $this->roleCreated = $roleCreated;
         return $this;
@@ -160,19 +222,69 @@ class Assessor extends Model
      * @param \DateTime|null $roleUpdated
      * @return Assessor
      */
-    public function setRoleUpdated(?\DateTime $roleUpdated) : Assessor
+    public function setRoleUpdated(?\DateTime $roleUpdated): Assessor
     {
         $this->roleUpdated = $roleUpdated;
         return $this;
     }
 
     /**
-     * @return Assessor
      * @param \DateTime|null $roleDeactived
+     * @return Assessor
      */
-    public function setRoleDeactivated(?\DateTime $roleDeactived) : Assessor
+    public function setRoleDeactivated(?\DateTime $roleDeactived): Assessor
     {
         $this->roleDeactivated = $roleDeactived;
         return $this;
     }
- }
+
+    /**
+     * @param string $city
+     * @return Assessor
+     */
+    public function setCity($city): Assessor
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @param string $state
+     * @return Assessor
+     */
+    public function setState($state): Assessor
+    {
+        $this->state = strtoupper($state);
+        return $this;
+    }
+
+    /**
+     * @param string $zip
+     * @return Assessor
+     */
+    public function setZip($zip): Assessor
+    {
+        $this->zip = $zip;
+        return $this;
+    }
+
+    /**
+     * @param string $phone
+     * @return Assessor
+     */
+    public function setPhone($phone): Assessor
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @param string $website
+     * @return Assessor
+     */
+    public function setWebsite($website): Assessor
+    {
+        $this->website = $website;
+        return $this;
+    }
+}
