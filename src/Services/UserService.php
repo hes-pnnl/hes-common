d8e948d28fcc8c60916187cff1e951b4164da56b
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2020-04-29 18:03:16 -0700

    Refactored UserService classes to derive from a common base class in hes-common, so that we don't have the same set of constants being (inconsistently) defined in two places

---
+++ src/Services/UserService.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace HESCommon\Services;
+
+
+class UserService extends Service
+{
+    /**
+     * ROLE_* constants represent roles that a user can have assigned to them. These must also be present in the
+     * Admin DB's `role` table.
+     */
+    const ROLE_ASSESSOR = 'assessor';
+    const ROLE_ADMIN = 'administrator';
+    const ROLE_PARTNER = 'partner';
+    const ROLE_MENTOR = 'mentor';
+    const ROLE_MENTEE = 'mentee_assessor';
+    const ROLE_QA = 'qa';
+    const ROLE_TRAINING_ADMIN = 'training_administrator';
+
+    /**
+     * @return array
+     */
+    public function getValidRoles() : array
+    {
+        return [ self::ROLE_ADMIN, self::ROLE_ASSESSOR, self::ROLE_PARTNER, self::ROLE_MENTOR, self::ROLE_MENTEE, self::ROLE_QA ];
+    }
+}
