d8a31944afa88ab9817c5e0305d377b9a854f9b2
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2020-06-02 12:32:05 -0700

    Added additional ROLE_* constants for the new roles that are automatically determined for each assessor

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -7,8 +7,8 @@ namespace HESCommon\Services;
 class UserService extends Service
 {
     /**
-     * ROLE_* constants represent roles that a user can have assigned to them. These must also be present in the
-     * Admin DB's `role` table.
+     * ROLE_* constants represent roles that a user can have assigned to them.
+     * These must also be present in the Admin DB's `role` table.
      */
     const ROLE_ASSESSOR = 'assessor';
     const ROLE_ADMIN = 'administrator';
@@ -19,18 +19,28 @@ class UserService extends Service
     const ROLE_TRAINING_ADMIN = 'training_administrator';
 
     /**
-     * @return array
+     * These roles are special roles assigned to assessors, and are determined
+     * based on an assessor's training status and the time since the assessor
+     * last assessed an official home. They are not in the `role` table, but are
+     * instead calculated automatically by the users_roles_and_statuses view.
      */
-    public function getValidRoles() : array
-    {
-        return [
-            self::ROLE_ASSESSOR,
-            self::ROLE_ADMIN,
-            self::ROLE_PARTNER,
-            self::ROLE_MENTOR,
-            self::ROLE_MENTEE,
-            self::ROLE_QA,
-            self::ROLE_TRAINING_ADMIN
-        ];
-    }
+    const ROLE_CANDIDATE_ASSESSOR = 'candidate_assessor';
+    const ROLE_TRAINEE_ASSESSOR = 'trainee_assessor';
+    const ROLE_MENTEE_ASSESSOR = 'mentee_assessor';
+    const ROLE_LAPSED_ASSESSOR = 'lapsed_assessor';
+    const ROLE_INACTIVE_ASSESSOR = 'inactive_assessor';
+
+    /**
+     * Roles that can be assigned to an assessor - depending on their training
+     * status and the time since they last assessed a home, a user assigned the
+     * "Assessor" role in the GUI will actually be assigned one of these roles.
+     */
+    const ASSESSOR_ROLES = [
+        self::ROLE_ASSESSOR,
+        self::ROLE_CANDIDATE_ASSESSOR,
+        self::ROLE_TRAINEE_ASSESSOR,
+        self::ROLE_MENTEE_ASSESSOR,
+        self::ROLE_LAPSED_ASSESSOR,
+        self::ROLE_INACTIVE_ASSESSOR
+    ];
 }
