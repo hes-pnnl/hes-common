47d16b431e2bbaa200557d1f0fc077f6217a1503
Yan Zhao <zhaoyan1555@gmail.com>
2023-08-05 13:57:40 -0700

    JSON object to array for roof& floor & HVAC

--- src/Services/BuildingService.php
+++ src/Services/BuildingService.php
@@ -91,6 +91,34 @@ class BuildingService
             $response['zone']['zone_wall'][$side] = $wall;
             unset($response['zone']['zone_wall'][$key]);
         }
+        if(array_key_exists('roof_name', $response['zone']['zone_roof'])){
+            $response['zone']['zone_roof'] = array($response['zone']['zone_roof']);
+        }
+
+        if(array_key_exists('floor_name', $response['zone']['zone_floor'])){
+            $response['zone']['zone_floor'] = array($response['zone']['zone_floor']);
+        }
+
+        if(
+            array_key_exists('systems', $response) &&
+            array_key_exists('hvac', $response['systems'])
+        ) {
+
+            if(array_key_exists('hvac_distribution', $response['systems']['hvac'])){
+                $response['systems']['hvac'] = array($response['systems']['hvac']);
+            }
+            foreach($response['systems']['hvac'] as $i => $hvac) {
+                if(
+                array_key_exists('hvac_distribution', $response['systems']['hvac'][$i])) {
+                    if (array_key_exists('hvac_duct', $response['systems']['hvac'][$i]['hvac_distribution'])) {
+                        $response['systems']['hvac'][$i]['hvac_distribution']['duct'] = $hvac['hvac_distribution']['hvac_duct'];
+                    }
+                    if (array_key_exists('location', $response['systems']['hvac'][$i]['hvac_distribution']['duct'])) {
+                        $response['systems']['hvac'][$i]['hvac_distribution']['duct'] = array($response['systems']['hvac'][$i]['hvac_distribution']['duct']);
+                    }
+                }
+            }
+        }
 
         /**
          * Sets a property in $building from a value in $response
